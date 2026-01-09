daff8b7bee5ff595286f160645b513e57a90168c
muazjamil <imuazjamil@gmail.com>
2026-01-09 18:44:56 +0500

    corrected twosum

--- twoSum.js
+++ twoSum.js
@@ -12,6 +12,11 @@ var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++)
     {
         const complement = target - nums[i];
+        if(complement == 0)
+        {
+            console.log("Number " + nums[i] + " at index " + i + " is target itself");
+            return 0;
+        }
         if(map.has(complement))
         {
             return[ map.get(complement), i];
@@ -19,6 +24,10 @@ var twoSum = function(nums, target) {
         map.set(nums[i],i);  
     }
 };
+
+nums = [1,7,2,15];
+target = 9;
+console.log(twoSum(nums,target)); // [0,1]
 
 // with the help of two loops
 // time complexity: O(n^2)
@@ -35,3 +44,7 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+nums = [1,7,2,15];
+target = 9;
+console.log(twoSum(nums,target)); // [0,1]
